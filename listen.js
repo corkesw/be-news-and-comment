5762a1bb99e480e48622fa7b42523edc3fc538ac
corkesw <home@stephencorke.com>
2021-09-23 11:27:31 +0100

    add connection setup for heroku

--- listen.js
+++ listen.js
@@ -1,6 +1,9 @@
 
 const app = require('./app')
 
-app.listen(9090, () => {
-    console.log("App is listening")
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
+    if(err) throw err;
+    console.log(`App is listening on ${PORT}`)
 })
