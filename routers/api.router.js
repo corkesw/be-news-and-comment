98cd09f78dd2b0af46d8608335007fd75211b4c9
corkesw <home@stephencorke.com>
2021-09-23 11:59:24 +0100

    correct error on filepath in apiRouter

--- routers/api.router.js
+++ routers/api.router.js
@@ -8,7 +8,7 @@ const { articlesRouter } = require("./articles.router");
 apiRouter.get("/", (req, res, next) => {
 
   return fsPromise
-    .readFile('/Users/stephencorke/northcoders/backend/be-nc-news/endpoints.json', "utf-8")
+    .readFile('./endpoints.json', "utf-8")
     .then((endpoints) => {
       res.status(200).send({endpoints});
     })
